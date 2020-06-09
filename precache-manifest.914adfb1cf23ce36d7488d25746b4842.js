dfa56633e6cad315dd52243120bbe5b9269b85b5
AhmedNasser-Front-End <ahmednasser_8@yahoo.com>
2020-06-10 01:57:20 +0200

    Updates

---
+++ precache-manifest.914adfb1cf23ce36d7488d25746b4842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dc622e3fd621656c3f621c83864fa5",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "39700e40bbc55e3f5235",
+    "url": "/todo/static/css/main.f539ebcb.chunk.css"
+  },
+  {
+    "revision": "e15610c1fad1053a8042",
+    "url": "/todo/static/js/2.ec0d45b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.ec0d45b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39700e40bbc55e3f5235",
+    "url": "/todo/static/js/main.a7657927.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
